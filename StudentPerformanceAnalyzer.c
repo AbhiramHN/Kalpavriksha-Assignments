2500d9e7f8b2337e3c7f42965b39024c716f0363
AbhiramHN <abhiramhn.33@gmail.com>
2025-10-17 20:41:58 +0530

    Updated the function and variable names, and fixed issue with mmultiple return statement

--- StudentPerformanceAnalyzer.c
+++ StudentPerformanceAnalyzer.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-struct student
+struct Student
 {
     int rollNumber;
     char name[20];
@@ -9,101 +9,104 @@ struct student
 
 char GradeCalculator(float averageMarks)
 {
+    char grade;
     if (averageMarks >= 85)
     {
-        return 'A';
+        grade = 'A';
     }
     else if (averageMarks >= 70)
     {
-        return 'B';
+        grade = 'B';
     }
     else if (averageMarks >= 50)
     {
-        return 'C';
+        grade = 'C';
     }
     else if (averageMarks >= 35)
     {
-        return 'D';
+        grade = 'D';
     }
     else
     {
-        return 'F';
+        grade = 'F';
     }
+    return grade;
 }
 
-const char *getStar(char grade)
+const char *GetStar(char grade)
 {
+    const char *stars;
     if (grade == 'A')
     {
-        return "*****";
+        stars = "*****";
     }
     else if (grade == 'B')
     {
-        return "****";
+        stars = "****";
     }
     else if (grade == 'C')
     {
-        return "***";
+        stars = "***";
     }
     else if (grade == 'D')
     {
-        return "**";
+        stars = "**";
     }
     else
     {
-        return NULL;
+        stars = NULL;
     }
+    return stars;
 }
 
-void printRollNumbers(struct student s[], int n, int index)
+void PrintRollNumbers(struct Student students[], int numberOfStudents, int index)
 {
-    if (index == n)
+    if (index == numberOfStudents)
     {
         return;
     }
-    printf("%d ", s[index].rollNumber);
-    printRollNumbers(s, n, index + 1);
+    printf("%d ", students[index].rollNumber);
+    PrintRollNumbers(students, numberOfStudents, index + 1);
 }
 
 int main()
 {
-    struct student Student[100];
+    struct Student students[100];
     int numberOfStudents;
 
     scanf("%d", &numberOfStudents);
 
     for (int index = 0; index < numberOfStudents; index++)
     {
-        scanf("%d %s %d %d %d", &Student[index].rollNumber, Student[index].name, &Student[index].mark1, &Student[index].mark2, &Student[index].mark3);
+        scanf("%d %s %d %d %d", &students[index].rollNumber, students[index].name, &students[index].mark1, &students[index].mark2, &students[index].mark3);
     }
 
-    
     for (int i = 0; i < numberOfStudents - 1; i++)
     {
         for (int j = i + 1; j < numberOfStudents; j++)
         {
-            if (Student[i].rollNumber > Student[j].rollNumber)
+            if (students[i].rollNumber > students[j].rollNumber)
             {
-                struct student temp = Student[i];
-                Student[i] = Student[j];
-                Student[j] = temp;
+                struct Student temp = students[i];
+                students[i] = students[j];
+                students[j] = temp;
             }
         }
     }
 
     for (int index = 0; index < numberOfStudents; index++)
     {
-        int totalMarks = Student[index].mark1 + Student[index].mark2 + Student[index].mark3;
+        int totalMarks = students[index].mark1 + students[index].mark2 + students[index].mark3;
         float averageMarks = totalMarks / 3.0;
         char grade = GradeCalculator(averageMarks);
 
-        printf("\nRoll: %d\n", Student[index].rollNumber);
-        printf("Name: %s\n", Student[index].name);
+        printf("\nRoll: %d\n", students[index].rollNumber);
+        printf("Name: %s\n", students[index].name);
         printf("Total: %d\n", totalMarks);
         printf("Average: %.2f\n", averageMarks);
         printf("Grade: %c\n", grade);
 
-        const char *stars = getStar(grade);
+        const char *stars = GetStar(grade);
         if (stars != NULL)
         {
             printf("Performance: %s\n", stars);
@@ -111,7 +114,7 @@ int main()
     }
 
     printf("\nList of Roll Numbers (via recursion): ");
-    printRollNumbers(Student, numberOfStudents, 0);
+    PrintRollNumbers(students, numberOfStudents, 0);
     printf("\n");
 
     return 0;
